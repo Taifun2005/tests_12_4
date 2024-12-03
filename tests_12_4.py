15e9c27c15fcb021e5be389cdab035cccb95f2c4
Taifun2005 <lider2005@yandex.ru>
2024-12-03 07:24:38 +0700

    Add files via upload

--- tests_12_4.py
+++ tests_12_4.py
@@ -1,12 +1,16 @@
 import logging
+from rt_with_exceptions import Runner
+logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='utf-8',
+                    format="%(asctime)s | %(levelname)s | %(message)s")
+
 class RunnerTest:
     @staticmethod
     def test_walk():
         try:
-            runner = Runner("Test Runner", speed=-5)
+            runner = Runner("Test Runner", speed=-10)
             runner.walk()
             logging.info(f'"test_walk" выполнен успешно')
-        except:
+        except ValueError :
             logging.warning(f"Неверная скорость для Runner")
 
     @staticmethod
@@ -15,9 +19,11 @@ class RunnerTest:
             runner = Runner(True, speed=10)
             runner.walk()
             logging.info(f'"test_run" выполнен успешно')
-        except:
+        except TypeError:
             logging.warning(f"Неверный тип данных для объекта Runner")
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='utf-8',
-                        format='%(asctime)s | %(levename)s | %(massage)s')
+    test = RunnerTest()
+    test.test_walk()
+    test.test_run()
+
